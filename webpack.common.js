eec213c272714614ce5efa9dde7301be96839e3f
Aayush Rai <coolaayushrai@gmail.com>
2024-06-13 23:03:55 +0530

    Install and apply loaders for CSS

--- webpack.common.js
+++ webpack.common.js
@@ -16,6 +16,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
                 test: /\.(jpg|jpeg|svg|png|gif)$/i,
                 type: 'asset/resource',
             },
