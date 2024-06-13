67d82f0bc736cc0b62719951325695eb5c18a0aa
Aayush Rai <coolaayushrai@gmail.com>
2024-06-13 23:20:29 +0530

    Apply tab-switching logic for the webpage
    The file imports the DOM content generated via different JS
    modules in order to display them as needed.
    
    The webpage shows the Home content by default.
    
    An event listener has been attached to nav in order to show the
    desired page depending upon the tab clicked by user.
    The tab is determined by the class attribute of the click
    event target.
    
    Since the main element has only one child attached, it is removed
    each time the user clicks a tab with the help of 'parent.lastChild'
    selector.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello Blue!");
+import home from './home.js';
+import menu from './menu.js';
+import about from './about.js';
+
+const content = document.querySelector('#content');
+content.appendChild(home());
+
+const nav = document.querySelector('nav');
+nav.addEventListener('click', (evnt) => {
+    switch (evnt.target.classList[0])
+    {
+        case 'home-btn':
+            content.removeChild(content.lastChild);
+            content.appendChild(home());
+            break;
+        case 'menu-btn':
+            content.removeChild(content.lastChild);
+            content.appendChild(menu());
+            break;
+        case 'about-btn':
+            content.removeChild(content.lastChild);
+            content.appendChild(about());
+        default:
+            break;
+    }
+});
