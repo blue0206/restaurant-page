283405f13e0f9310f661279f99f35c7c48146959
Aayush Rai <coolaayushrai@gmail.com>
2024-06-14 16:38:10 +0530

    Add logic to change tab color on click

--- src/index.js
+++ src/index.js
@@ -8,18 +8,31 @@ const content = document.querySelector('#content');
 content.appendChild(home());
 
 const nav = document.querySelector('nav');
+const tabList = document.querySelectorAll('nav > button');
 nav.addEventListener('click', (evnt) => {
     switch (evnt.target.classList[0])
     {
         case 'home-btn':
+            tabList[1].style.backgroundColor = "hsla(258, 100%, 67%, 0.418)";
+            tabList[2].style.backgroundColor = "hsla(258, 100%, 67%, 0.418)";
+            evnt.target.style.backgroundColor = "hsla(225, 46%, 95%, 0.842)";
+
             content.removeChild(content.lastChild);
             content.appendChild(home());
             break;
         case 'menu-btn':
+            tabList[0].style.backgroundColor = "hsla(258, 100%, 67%, 0.418)";
+            tabList[2].style.backgroundColor = "hsla(258, 100%, 67%, 0.418)";
+            evnt.target.style.backgroundColor = "hsla(225, 46%, 95%, 0.842)";
+
             content.removeChild(content.lastChild);
             content.appendChild(menu());
             break;
         case 'about-btn':
+            tabList[0].style.backgroundColor = "hsla(258, 100%, 67%, 0.418)";
+            tabList[1].style.backgroundColor = "hsla(258, 100%, 67%, 0.418)";
+            evnt.target.style.backgroundColor = "hsla(225, 46%, 95%, 0.842)";
+
             content.removeChild(content.lastChild);
             content.appendChild(about());
         default:
